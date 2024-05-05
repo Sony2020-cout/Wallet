3636abb03a3e18f37e314c7f7e1691f125598baa
Sony <programmer.2020@mail.ru>
2024-05-05 13:08:50 +0300

    Добавление изменения и поиска

--- Wallet.py
+++ Wallet.py
@@ -1,4 +1,4 @@
-
+import mmap
 def record(total):
     with open('wallet.txt', 'w') as txt:
         txt.write(str(total))
@@ -15,6 +15,21 @@ def readd(operation):
             description = input('Описание: ')
             date1 = input('Дата: ')
             record(str(txt) + '\n' +'Дата: ' +date1 + '\n' +'Категория: ' + category + '\n' + 'Сумма: '  + howMany + '\n' + 'Описание: ' +description)
+        elif operation == 'п':
+            with open('wallet.txt') as f:
+                s = mmap.mmap(f.fileno(),0, access=mmap.ACCESS_READ)
+                if s.find('кря') != -1:
+                    print('true')
+                elif s.find('Покупка гаджета') != -1:
+                    print('true')
+
+        elif operation == 'и':
+            s = input('Введите текст для замены: ')
+            f = open('wallet.txt', 'r+')
+            f.truncate(0)
+            f.write(s)
+            f.close()
+            print('Текст изменён')
         elif operation == 'в':
             quit()
 
